2ba3b439252bc0b3c75f0a27f64ad982a1ec78cb
Jarren San Jose <sanjose.jarren@gmail.com>
2023-12-11 14:43:52 -0800

    add shirt overlay

--- components/Seal.tsx
+++ components/Seal.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import styles from "./Seal.module.css";
 
 const Seal = () => {
   return (
-    <div>
+    <div className={styles.imageContainer}>
       <Image src="https://ipfs.io/ipfs/QmUs4WQP47QKGwzPLjVMmhqTbspJfAC344abDEE2UT52HF/6024.png" alt="sappy seal" width={200} height={200} />
+      <Image src="/sealTeeOverlay.png" alt="overlay image" layout="fill" objectFit="cover" />
     </div>
   )
 }
