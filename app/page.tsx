68b47441ab674479a71f380e9e06f84cafdc2fcc
Jarren San Jose <sanjose.jarren@gmail.com>
2023-12-11 13:59:41 -0800

    add pasta

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-6xl font-bold text-center">Sappy Tees</h1>
+      <h1 className="text-6xl font-bold text-center mb-4">Sappy Tees</h1>
+      <p className="text-sm text-center">
+        oversized dropped shoulder 280 gsm 8.2oz vintage retro inspired heavyweight luxury limited edition 24 hour exclusive sappy seal t-shirt
+      </p>
     </main>
   )
 }
